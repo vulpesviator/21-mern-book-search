b67fcc089a0c19242252d06496e47ed6ab6b358d
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-08-25 16:02:38 -0500

    Added me & login resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,10 +3,34 @@ const { signToken } = require('../utils/auth')
 
 const resolvers = {
   Query: {
-    
+    me: async (parent, args, context) => {
+      console.log(context.user)
+
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+
+      throw new AuthenticationError('No user found with this email address');
+    },
   },
   Mutation: {
-    
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
   },
 };
 
