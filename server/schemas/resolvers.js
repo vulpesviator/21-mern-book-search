8ffd9638ae7d0e9cdf5476be7720f4f1891df78f
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-08-25 09:34:07 -0500

    Added typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth')
+
+const resolvers = {
+  Query: {
+    
+  },
+  Mutation: {
+    
+  },
+};
+
+module.exports = resolvers;
